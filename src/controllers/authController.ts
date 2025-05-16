f6c53fc459bfa860ef45ddf9c7d43d605c5692bd
kaungkhantsoe <kaungkhants892@gmail.com>
2025-05-16 21:23:13 +0630

    removingMocking

--- src/controllers/authController.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from "express";
-
-const AuthController = {
-  index: async (req, res): Promise<Response> => {
-    return res.json({
-      data: "authenicated succesfully",
-    });
-  },
-};
